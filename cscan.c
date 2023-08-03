00e9a6151b0928ffe4339dab3108772f3d0b239e
karthik Gowda <128509521+karthikgowdaa@users.noreply.github.com>
2023-08-03 12:02:30 +0530

    Create cscan.c

---
+++ cscan.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int RQ[100],i,j,n,TotalHeadMoment=0,initial,size,move;
+    printf("enter the number of requests\n");
+    scanf("%d",&n);
+    printf("enter the request sequence\n");
+    for(i=0;i<n;i++)
+    scanf("%d",&RQ[i]);
+    printf("enter the intitial head position\n");
+    scanf("%d",&initial)
+}
